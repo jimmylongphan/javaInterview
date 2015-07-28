7d817b08ec3a598d45861188e6ff4a4d00b0cabd
suprasaiyajim <phanjim@gmail.com>
2015-07-28 07:29:49 +0000

    setup pom

--- javainterviewapp/src/main/java/com/javainterview/app/arrays/ReverseString.java
+++ javainterviewapp/src/main/java/com/javainterview/app/arrays/ReverseString.java
@@ -4,16 +4,15 @@
 
 public class ReverseString {
 
-    /**
-     *
-     */
+      /**
+       */
     public String reverseString( String s ) {
         if ( s == null || s.isEmpty() ) {
             return s;
         }
 
         String result = "";
-        for ( int i=s.length - 1; i>=0 ; i--) {
+        for ( int i=s.length() - 1; i>=0 ; i--) {
             result = result + s.charAt(i);
         }
 
@@ -28,14 +27,14 @@ public class ReverseString {
     public String reverseStringInPlace( String s ) {
         if ( s == null || s.isEmpty() ) {
             return s;
-        } else if ( s.length == 1 ) {
+        } else if ( s.length() == 1 ) {
             return s;
         }
 
         // There is no putChar method for java String
         char temp;
-        char[] array = new char[s.length];
-        for ( int i=0, j=s.length -1; i<s.length/2; i++, j--) {
+        char[] array = new char[s.length()];
+        for ( int i=0, j=s.length() -1; i<s.length()/2; i++, j--) {
             temp = s.charAt(i);
             array[i] = s.charAt(j);
             array[j] = temp;
