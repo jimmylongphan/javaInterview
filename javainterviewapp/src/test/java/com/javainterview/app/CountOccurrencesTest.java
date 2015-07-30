80015dce28f4a986c52772e207d9b11dcf6c1f42
suprasaiyajim <phanjim@gmail.com>
2015-07-30 01:48:11 +0000

    added count occurrences

---
+++ javainterviewapp/src/test/java/com/javainterview/app/CountOccurrencesTest.java
@@ -0,0 +1,33 @@
+
+import static org.testng.Assert.*;
+
+import org.testng.annotations.Test;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.BeforeTest;
+
+import CountOccurrences.CountOccurrences;
+
+public class CountOccurrencesTest {
+    
+    CountOccurrences countOccurrences;
+    
+	@BeforeSuite
+	public void testBeforeSuite() {
+		System.out.println("testBeforeSuite() creating countOccurrences");
+		countOccurrences = new CountOccurrences();
+	}
+	
+	@Test
+	public void checkThree() {
+	    int[] input = { 3, 3, 3 };
+	    assertTrue(countOccurrences.hasNOccurrences(input, 3));
+	}
+	
+	@Test
+	public void checkFiveFail() {
+	    int[] input = { 1 };
+	    assertFalse(countOccurrences.hasNOccurrences(input, 5));
+	}
+}
