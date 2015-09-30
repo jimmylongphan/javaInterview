e26ab6494c24014a2ea707fca8320ea61b1f5372
suprasaiyajim <phanjim@gmail.com>
2015-09-30 09:26:56 +0000

    tree tests

--- javainterviewapp/src/main/java/com/javainterview/app/treeInterview/BinaryTree.java
+++ javainterviewapp/src/main/java/com/javainterview/app/treeInterview/BinaryTree.java
@@ -2,10 +2,20 @@ package com.javainterview.app.treeInterview;
 
 public class BinaryTree {
 
+    public Node root;
+    
+    public BinaryTree(int value) {
+        root = new Node(value);
+    }
+
     public class Node {
         public int value;
         public Node left;
         public Node right;
+        
+        public Node(int value) {
+            this.value = value;
+        }
     }
 
     public boolean isBST(Node root, Integer min, Integer max) {
@@ -34,12 +44,77 @@ public class BinaryTree {
         return right;
     }
 
-    public void insert(Node root) {
-
+    /**
+     * Naive algorithm is to check if the value is less than or equal than the current node
+     * If it is less, then recursively insert left.
+     * If it is greater, then recurisvely insert right.
+     * 
+     * Always insert at a leaf position.
+     */
+    public Node insert(Node root, int value) {
+        if( root == null ) {
+            root = new Node(value);
+            return root;
+        }
+        
+        if( value < root.value ) {
+            root.left = insert(root.left, value);
+        } else if ( value > root.value) {
+            root.right = insert(root.right, value);
+        }
+        return root;
     }
 
-    public void delete(Node root) {
-
+    public boolean delete(Node root, int value) {
+        if ( root == null ) {
+            return false;
+        }
+        
+        if( value < root.value ) {
+            delete( root.left, value );
+        } else if ( value > root.value ) {
+            delete( root.right, value);
+        } else {
+            // handle 2 children case
+            if( root.left != null && root.right != null) {
+                // find the greatest child on the left side
+                Node maxLeft = findMax(root.left);
+                
+                // replace the current value with the max from left
+                root.value = maxLeft.value;
+                
+                // Remove the Node that we just put in this position
+                delete(maxLeft.value, root.left);
+            } else if(root.left != null) {
+                // handle left child
+                Node throwaway = root;
+                
+                // replace the current node with left
+                root = root.left;
+                throwaway = null;
+            } else if( root.right != null) {
+                // handle right child
+                Node throwaway = root;
+                
+                // replace current node with right
+                root = root.right;
+                throwaway = null;
+            } else {
+                // no children so just remove node
+                root = null;
+            }
+        }
     }
 
+    /**
+     * Find the greatest node from this node
+     */
+    public Node findMax(Node root) {
+        if( root.right != null ) {
+            return findMax(root.right);
+        }
+        
+        // no greater nodes so return this node
+        return root;
+    }
 }
--- javainterviewapp/src/main/java/com/javainterview/app/treeInterview/Trie.java
+++ javainterviewapp/src/main/java/com/javainterview/app/treeInterview/Trie.java
@@ -2,6 +2,26 @@ package com.javainterview.app.treeInterview;
 
 /**
  * Created on 9/28/2015.
+ * http://www.programcreek.com/2014/05/leetcode-implement-trie-prefix-tree-java/
+ * 
  */
 public class Trie {
+    
+    public class TrieNode {
+        public char c;
+        public HashMap<Character, TrieNode> children = new HashMap<Character, TrieNode>();
+        public boolean isLeaf;
+        
+        public TrieNode() {};
+        
+        public TrieNode(char c) {
+            this.c = c;
+        }
+    }
+    
+    private TrieNode root;
+    
+    public Trie() {
+        root = new TrieNode();
+    }
 }
